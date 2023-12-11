882123bac06dd26eb755162adc07329e853bcedb
Kevin Egstorf <kevin.egstorf@gmail.com>
2023-12-11 20:09:17 +0100

    feat: created form

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  @Input() todo: any = { id: '', title: '', completed: false }; // Input to receive todo data
+  @Output() saveTodo = new EventEmitter<any>(); // Output to emit the saved todo
+
+  onSubmit() {
+    this.saveTodo.emit(this.todo);
+  }
+}
